d91a7ba1d19dfa9fc5cd67f071601f7b6d627fe0
Seppe Staelens <seppe.staelens@student.kuleuven.be>
2024-09-04 13:39:55 +0100

    remove obsolete

--- Source/ApparentHorizonFinder/ApparentHorizon.impl.hpp
+++ Source/ApparentHorizonFinder/ApparentHorizon.impl.hpp
@@ -81,7 +81,6 @@ template <class SurfaceGeometry, class AHFunction>
 bool ApparentHorizon<SurfaceGeometry, AHFunction>::good_to_go(
     double a_dt, double a_time) const
 {
-    pout() << "In [ApparentHorizon::good_to_go]" << std::endl;
     if (a_time < m_params.start_time - 1.e-7 /*just to be safe*/)
         return false;
     if (m_params.give_up_time >= 0. && a_time >= m_params.give_up_time &&
@@ -93,17 +92,7 @@ bool ApparentHorizon<SurfaceGeometry, AHFunction>::good_to_go(
              ? m_num_failed_convergences > m_params.max_fails_after_lost
     	     : false);
 
-    pout() << "is lost = " << is_lost << std::endl;
-    pout() << "has been found = " << has_been_found() << std::endl;
-
     // stop if it has been found but was lost
-    if (is_lost && has_been_found())
-        pout() << "Horizon was lost. Stopping." << std::endl;
-    else if (do_solve(a_dt, a_time))
-        pout() << "do_solve(a_dt, a_time) = true" << std::endl;
-    else
-        pout() << "do_solve(a_dt, a_time) = false" << std::endl;
-
     return do_solve(a_dt, a_time) && !(has_been_found() && is_lost);
 }
 
